8f2de021b273439e29049e18740dc568bf510333
SailorDwayne <bjensen3@live.com>
2017-01-11 12:23:41 -0700

    Finished main part of program

--- Bradens_Folder/boop.py
+++ Bradens_Folder/boop.py
@@ -1,9 +1,10 @@
+from __future__ import print_function
 import PIL
 from PIL import Image
 import os  
 directory = os.path.dirname(os.path.realpath(__file__))
 new_directory = os.path.join(directory, 'modified')
-im = Image.open("oxiclean orange bar.png") 
+im = Image.open("oxicleanorangebar.png") 
 directory_list = os.listdir(directory)
 
 def get_images(directory=None):
@@ -11,21 +12,15 @@ def get_images(directory=None):
     file_list = []
     if directory == None:
         directory = os.getcwd() 
-        for entry in directory_list:
-            absolute_filename = os.path.join(directory, entry)
-            try:
-                image = PIL.Image.open(absolute_filename)
-                file_list += [entry]
-                image_list += [image]
-            except IOError:
-                pass 
-        return image_list, file_list          
-
-def paste_image():
-    filename,filetype = os.path.splitext
-    new_image = im.past(im,(20,20))
-    new_image_filename = os.path.join(new_directory, filename + '.png')
-    new_image.save(new_image_filename)    
+    for entry in directory_list:
+        absolute_filename = os.path.join(directory, entry)
+        try:
+            image = PIL.Image.open(absolute_filename)
+            file_list += [entry]
+            image_list += [image]
+        except IOError:
+            pass 
+    return image_list, file_list          
 
 def paste_on_all_images(directory=None):
     if directory == None:
@@ -35,9 +30,10 @@ def paste_on_all_images(directory=None):
         os.mkdir(new_directory)
     except OSError:
         pass
-    image_list, file_list = get_images(directory)     
-    for n in range(len(image_list)):
+    image_list,file_list = get_images(directory) 
+    for n in range(len(file_list)):
+        new_image= image_list[n]
+        new_image.paste(im,(20,20))
         filename, filetype = os.path.splitext(file_list[n])
-        new_image = paste_image()
         new_image_filename = os.path.join(new_directory, filename + '.png')
         new_image.save(new_image_filename)    
